e0e487544c791d6227449412186d4a6ef7b2d483
lekan <laksidesvibe@gmail.com>
2023-04-30 03:39:32 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,47 @@
 /* Your Code Here */
 
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => ({
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  });
+  
+  const createEmployeeRecords = (array) => array.map(createEmployeeRecord);
+  
+  const createTimeEvent = (type) => function(dateStamp) {
+    const [date, hour] = dateStamp.split(' ')
+    const timeEvent = {
+      type,
+      hour: parseInt(hour),
+      date,
+    };
+    this[type === "TimeIn" ? "timeInEvents" : "timeOutEvents"].push(timeEvent);
+    return this;
+  };
+  
+  const createTimeInEvent = createTimeEvent("TimeIn");
+  const createTimeOutEvent = createTimeEvent("TimeOut");
+  
+  const hoursWorkedOnDate = function(date) {
+    const timeIn = this.timeInEvents.find((event) => event.date === date);
+    const timeOut = this.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  };
+  
+  const wagesEarnedOnDate = function(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+  };
+  
+  const findEmployeeByFirstName = (srcArray, firstName) =>
+    srcArray.find((employee) => employee.firstName === firstName);
+  
+  const calculatePayroll = (array) =>
+    array.reduce((total, employee) => total + allWagesFor.call(employee), 0);
+  
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
